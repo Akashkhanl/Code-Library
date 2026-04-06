dc24dae9b334800e1c489934897887cff1a8561e
Akashkhanl <162981460+Akashkhanl@users.noreply.github.com>
2026-04-06 23:25:37 +0600

    Add files via upload

---
+++ Graph/ok.cpp
@@ -0,0 +1,41 @@
+class TopologicaSort {
+
+    vector<int>kahnA(int n,vector<vector<int>>&adj) {
+        vector<int>inDegree(n,0);
+
+        for(int u =0;u<n;u++) {
+            for(int v:adj[u]) {
+                inDegree[v]++;
+            }
+        }
+        queue<int>q;
+
+        for(int i =0;i<n;i++) {
+            if(inDegree[i]==0) {
+                q.push(i);
+            }
+        }
+        vector<int>res;
+
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            res.push_back(u);
+
+
+            for(int v:adj[u]) {
+                inDegree[v]--;
+
+                if(inDegree[v]==0) {
+                    q.push(v);
+                }
+            }
+        }
+        if(res.size()!=n) return {};
+        return res;
+
+    }
+
+
+
+};
